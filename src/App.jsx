f067b77962b2600f53c1c45203f39ed7bbb9264d
Vaibhav <your-email@example.com>
2025-06-03 14:56:25 +0530

    ha ji suru karte h

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
    <div>
     <div className="navbar bg-base-200 shadow-sm">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">Dev-Tinder</a>
+    <a className="btn btn-ghost text-xl">Dev-Tinder front</a>
   </div>
   <div className="flex gap-2 mx-4">
     <div className="dropdown dropdown-end">
