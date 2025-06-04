a86305920ecd85dbfec50f17030da3b123a9a2b0
Vaibhav <your-email@example.com>
2025-06-04 11:30:40 +0530

    Add Footer component and integrate lucide-react icons in Login and Navbar

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Footer from './Footer'
 import { Outlet } from 'react-router-dom'
 
 const Body = () => {
   return (
     <div>
   <Navbar/>
-  <Outlet></Outlet>
+  <Outlet/>
+  <Footer/>
         
     </div>
   )
